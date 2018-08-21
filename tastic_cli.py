91b75211fe706cbbeaaa42899155572c2a668ace
Unknown <lucia.conde@outlook.com>
2018-08-21 18:17:07 +0200

    Added new menu option 'test current model' + CLI/options class grouping
    Functions in CLI and options were grouped in classes for packaging.

--- tastic_cli.py
+++ tastic_cli.py
@@ -1,31 +1,38 @@
 import tastic_options
 
+opt = tastic_options.options(22050, 22050, 2, 1)
+
 def menu():
     print(10 * "-", "TASTIC: a machine learning-based music taste predictor", 10 * "-")
     print("1. New model")
     print("2. Continue model")
-    print("3. Delete all data")
-    print("4. Exit")
+    print("3. Test current model")
+    print("4. Delete all data")
+    print("5. Exit")
     print(76 * "-")
 
-app_open = True      
+def app():
+    app_open = True      
   
-while app_open:
-    menu()    # display menu
-    option = int(input("Enter your choice [1-4]: "))
+    while app_open:
+        menu()    # display menu
+        option = int(input("Enter your choice [1-5]: "))
      
-    if option == 1:     
-        print(5 * "-", "Creating new model...", 5 * "-")
-        tastic_options.createNewModel()
-    elif option == 2:
-        print(5 * "-", "Continuing model...", 5 * "-")
-        tastic_options.continueModel()
-    elif option == 3:
-        print(5 * "-", "Deleting all data...", 5 * "-")
-        tastic_options.deleteAllData()
-    elif option == 4:
-        print(5 * "-", "Closing Tastic...", 5 * "-")
-        app_open = False
-    else:
-        # Any integer inputs other than values 1-5 we print an error message
-        print("Wrong one, we don't have that many options! Please choose another one.")
+        if option == 1:     
+            print(5 * "-", "Creating new model...", 5 * "-")
+            opt.createNewModel()
+        elif option == 2:
+            print(5 * "-", "Continuing model...", 5 * "-")
+            opt.continueModel()
+        elif option == 3:
+            print(5 * "-", "Testing model...", 5 * "-")
+            opt.testModel()
+        elif option == 4:
+            print(5 * "-", "Deleting all data...", 5 * "-")
+            opt.deleteAllData()
+        elif option == 5:
+            print(5 * "-", "Closing Tastic...", 5 * "-")
+            app_open = False
+        else:
+            # Any integer inputs other than values 1-5 we print an error message
+            print("Wrong one, we don't have that many options! Please choose another one.")
--- tastic_options.py
+++ tastic_options.py
@@ -6,88 +6,96 @@ import model_testing
 import librosa
 import os
 
-def downloadSongs():
-    song_liked1 = input("Enter a YouTube link to a song you like: ")
-    song_liked2 = input("Great! Enter a YouTube link to a second song you like: ")
-    song_disliked1 = input("Almost there! Enter a YouTube link to a song you dislike: ")
-    song_disliked2 = input("And finally, enter a YouTube link to a second song you dislike: ")
-
-    # download all songs
-    print("Downloading songs...")
-    song_download.downloadSong(song_liked1, True)
-    song_download.downloadSong(song_liked2, True)
-    song_download.downloadSong(song_disliked1, False)
-    song_download.downloadSong(song_disliked2, False)
-
-def createModel():
-    # parameters for training data
-    sr_train = 22050
-    winsize_train = 1
-    # parameters for testing data
-    sr_test = 22050 # sampling rate of the song audio file to be tested
-    winsize_test = 2 # size of window (in seconds) for the division in frames of the audio signal
-    # predictions are run on each frame
-    # the class that was predicted the most is chosen as the overall prediction for the whole song
-
-    # load all the audio (liked, disliked) and perform feature extraction
-    ap = audio_processing.AudioProcessor(sr_train, winsize_train)
-    features_liked, features_disliked = ap.extractAllFeatures()
-
-    # train the random regression forest
-    print("Creating the predictive model...")
-    mt = model_training.ModelTrainer(features_liked, features_disliked, ap.n_samples_liked, ap.n_samples_disliked, ap.winsize_f)
-    model = mt.fitRRForest()
-    print("Model created.")
-
-    print("The predictive model is ready!")
-    song_test = input("Now, enter a YouTube link to any song you want me to predict your taste on: ")
-
-    # load the test song and extract its features
-    song_download.downloadTestSong(song_test)
-    print("Processing test song audio...")
-    at = audio_processing.AudioTester(sr_test, winsize_test)
-    features_test = at.extractAllFeatures()
-    print("Test song processed.")
-
-    # run the model for the test song
-    mtest = model_testing.ModelTester(features_test, model, at.title)
-    print("DONE! The overall prediction is: ",mtest.predictTaste())
-
-    # verify prediction and move test song to training data
-    true_taste = input("Was that right? Can you tell me if you actually liked or disliked that song? [write like or dislike]: ")
-    mtest.verifyPrediction(true_taste)
-
-def createNewModel():
-    print("Hi there! Let's get to know your music taste...")
-    downloadSongs()
-    createModel()
-
-def continueModel():
-    if verifyExistingData():
-        createModel()
-
-def verifyExistingData():
-    files_like = librosa.util.find_files('like/', ext=['mp3'])
-    files_dislike = librosa.util.find_files('dislike/', ext=['mp3'])
-
-    if not files_like or not files_dislike:
-        print("There are not enough stored songs to continue building a model!")
-        return False
-    else:
-        return True
+class options():
+    def __init__(self, sr_train, sr_test, winsize_train, winsize_test):
+        currentModel = None
+        # parameters for training data
+        # sr = sampling rate
+        # winsize = size of window (in seconds) for the division in frames of the audio signal
+        self.sr_train = sr_train
+        # parameters for testing data
+        self.winsize_train = winsize_train
+        self.sr_test = sr_test
+        self.winsize_test = winsize_test
+        
+        # predictions are run on each frame
+        # the class that was predicted the most is chosen as the overall prediction for the whole song
 
 
-def deleteAllData():
-    files_like = librosa.util.find_files('like/', ext=['mp3'])
-    files_dislike = librosa.util.find_files('dislike/', ext=['mp3'])
-    files_test = librosa.util.find_files('test/', ext=['mp3'])
+    def downloadSongs(self):
+        song_liked1 = input("Enter a YouTube link to a song you like: ")
+        song_liked2 = input("Great! Enter a YouTube link to a second song you like: ")
+        song_disliked1 = input("Almost there! Enter a YouTube link to a song you dislike: ")
+        song_disliked2 = input("And finally, enter a YouTube link to a second song you dislike: ")
 
-    files_all = files_like + files_dislike + files_test
+        # download all songs
+        print("Downloading songs...")
+        song_download.downloadSong(song_liked1, True)
+        song_download.downloadSong(song_liked2, True)
+        song_download.downloadSong(song_disliked1, False)
+        song_download.downloadSong(song_disliked2, False)
+
+    def testModel(self):
+        song_test = input("Now, enter a YouTube link to any song you want me to predict your taste on: ")
+
+        # load the test song and extract its features
+        song_download.downloadTestSong(song_test)
+        print("Processing test song audio...")
+        at = audio_processing.AudioTester(self.sr_test, self.winsize_test)
+        features_test = at.extractAllFeatures()
+        print("Test song processed.")
+
+        # run the model for the test song
+        mtest = model_testing.ModelTester(features_test, self.currentModel, at.title)
+        print("DONE! The overall prediction is: ",mtest.predictTaste())
+
+        # verify prediction and move test song to training data
+        true_taste = input("Was that right? Can you tell me if you actually liked or disliked that song? [write like or dislike]: ")
+        mtest.verifyPrediction(true_taste)
+
+    def createModel(self):
+        # load all the audio (liked, disliked) and perform feature extraction
+        ap = audio_processing.AudioProcessor(self.sr_train, self.winsize_train)
+        features_liked, features_disliked = ap.extractAllFeatures()
+
+        # train the random regression forest
+        print("Creating the predictive model...")
+        mt = model_training.ModelTrainer(features_liked, features_disliked, ap.n_samples_liked, ap.n_samples_disliked, ap.winsize_f)
+        self.currentModel = mt.fitRRForest()
+        print("The predictive model is ready!")
+
+    def createNewModel(self):
+        print("Hi there! Let's get to know your music taste...")
+        self.downloadSongs()
+        self.createModel()
+        self.testModel()
+
+    def continueModel(self):
+        if self.verifyExistingData():
+            self.createModel()
+        self.testModel()
+
+    def verifyExistingData(self):
+        files_like = librosa.util.find_files('like/', ext=['mp3'])
+        files_dislike = librosa.util.find_files('dislike/', ext=['mp3'])
+
+        if not files_like or not files_dislike:
+            print("There are not enough stored songs to continue building a model!")
+            return False
+        else:
+            return True
+
+    def deleteAllData(self):
+        files_like = librosa.util.find_files('like/', ext=['mp3'])
+        files_dislike = librosa.util.find_files('dislike/', ext=['mp3'])
+        files_test = librosa.util.find_files('test/', ext=['mp3'])
+
+        files_all = files_like + files_dislike + files_test
     
-    for y in files_all:
-        try:
-            os.remove(y)
-        except FileNotFoundError:
-            pass
-    print("Data deleted.")
+        for y in files_all:
+            try:
+                os.remove(y)
+            except FileNotFoundError:
+                pass
+        print("Data deleted.")
   
